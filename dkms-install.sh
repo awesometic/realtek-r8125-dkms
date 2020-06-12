e7688f2f7e46e1f82dc4a1dd9c24e13c07e642a8
Deokgyu Yang <secugyu@gmail.com>
2020-06-12 15:13:09 +0900

    Make DKMS working
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>

---
+++ dkms-install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+  echo "You must run this with superuser priviliges.  Try \"sudo ./dkms-install.sh\"" 2>&1
+  exit 1
+else
+  echo "About to run dkms install steps..."
+fi
+
+DRV_DIR="$(pwd)"
+DRV_NAME=r8125
+DRV_VERSION=9.003.05
+
+cp -r ${DRV_DIR} /usr/src/${DRV_NAME}-${DRV_VERSION}
+
+dkms add -m ${DRV_NAME} -v ${DRV_VERSION}
+dkms build -m ${DRV_NAME} -v ${DRV_VERSION}
+dkms install -m ${DRV_NAME} -v ${DRV_VERSION}
+RESULT=$?
+
+echo "Finished running dkms install steps."
+
+exit $RESULT
