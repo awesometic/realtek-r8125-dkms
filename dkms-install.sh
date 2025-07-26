5f74f2faa0c74bb86b448bd9fce38610b3f06018
timecode <rob.playford@gmail.com>
2025-07-26 09:19:21 +0100

    allow optional KERNEL_VERSION from ENV
    Use the KERNEL_VERSION environment variable if set; otherwise, use the current kernel version

--- dkms-install.sh
+++ dkms-install.sh
@@ -10,12 +10,13 @@ fi
 DRV_DIR="$(pwd)"
 DRV_NAME=r8125
 DRV_VERSION=9.015.00
+KERNEL_VERSION="${KERNEL_VERSION:-$(uname -r)}"
 
 cp -r ${DRV_DIR} /usr/src/${DRV_NAME}-${DRV_VERSION}
 
-dkms add -m ${DRV_NAME} -v ${DRV_VERSION}
-dkms build -m ${DRV_NAME} -v ${DRV_VERSION}
-dkms install -m ${DRV_NAME} -v ${DRV_VERSION}
+dkms add -m ${DRV_NAME} -v ${DRV_VERSION} -k ${KERNEL_VERSION}
+dkms build -m ${DRV_NAME} -v ${DRV_VERSION} -k ${KERNEL_VERSION}
+dkms install -m ${DRV_NAME} -v ${DRV_VERSION} -k ${KERNEL_VERSION}
 RESULT=$?
 
 echo "Finished running dkms install steps."
